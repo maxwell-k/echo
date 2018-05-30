4887db39676afaa42e270c5725ae12d674f20976
Keith Maxwell <keith.maxwell@gmail.com>
2018-05-30 12:54:09 +0100

    Package.json matches (6)

--- server.js
+++ server.js
@@ -43,5 +43,5 @@ var bot = new builder.UniversalBot(connector);
 bot.set("storage", tableStorage);
 
 bot.dialog("/", function(session) {
-  session.send("You said " + session.message.text + " (5)");
+  session.send("You said " + session.message.text + " (6)");
 });
